deef7551777eeccc60ff73f2b554ba5d6de7a356
Ach1L <roman.kanakov.3007@gmail.com>
2025-05-27 20:57:34 +0300

    Boost and GTest fix

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -3,36 +3,39 @@ include(ExternalProject)
 set(BOOST_SOURCE_DIR ${CMAKE_SOURCE_DIR}/3dparty/boost)
 
 if(MSVC)
-    set(BOOST_BOOTSTRAP ${BOOST_SOURCE_DIR}/bootstrap.bat)
-    set(BOOST_TOOLSET "msvc")
+set(BOOST_BOOTSTRAP ${BOOST_SOURCE_DIR}/bootstrap.bat)
+set(BOOST_TOOLSET "msvc")
 else()
-    set(BOOST_BOOTSTRAP ${BOOST_SOURCE_DIR}/bootstrap.sh)
-    set(BOOST_TOOLSET "clang")
+set(BOOST_BOOTSTRAP ${BOOST_SOURCE_DIR}/bootstrap.sh)
+set(BOOST_TOOLSET "clang")
 endif()
 
 set(BOOST_B2_COMMAND ${BOOST_SOURCE_DIR}/b2
-    --prefix=${CMAKE_CURRENT_BINARY_DIR}/chess_engine_boost/install
-    --build-dir=${CMAKE_CURRENT_BINARY_DIR}/chess_engine_boost/build
-	--without-mpi
-    toolset=${BOOST_TOOLSET}
-    address-model=64
-    variant=$<LOWER_CASE:$<CONFIG>>
-    link=static
-    runtime-link=shared
-    threading=multi
-    install
+--prefix=${CMAKE_CURRENT_BINARY_DIR}/chess_engine_boost/install
+--build-dir=${CMAKE_CURRENT_BINARY_DIR}/chess_engine_boost/build
+--without-mpi
+toolset=${BOOST_TOOLSET}
+address-model=64
+variant=$<LOWER_CASE:$<CONFIG>>
+link=static
+runtime-link=shared
+threading=multi
+--layout=system
+install
 )
 
 ExternalProject_Add(chess_engine_boost
-    SOURCE_DIR "${BOOST_SOURCE_DIR}"
-    PREFIX "${CMAKE_CURRENT_BINARY_DIR}/chess_engine_boost"
-    CONFIGURE_COMMAND "${BOOST_BOOTSTRAP}"
-    BUILD_COMMAND ${BOOST_B2_COMMAND}
-    INSTALL_COMMAND ""
-    BUILD_IN_SOURCE 1
+SOURCE_DIR ${BOOST_SOURCE_DIR}
+PREFIX ${CMAKE_CURRENT_BINARY_DIR}/chess_engine_boost
+CONFIGURE_COMMAND ${BOOST_BOOTSTRAP}
+BUILD_COMMAND ${BOOST_B2_COMMAND}
+INSTALL_COMMAND ""
+BUILD_IN_SOURCE 1
 )
 
-ExternalProject_Get_Property(chess_engine_boost INSTALL_DIR)
+ExternalProject_Get_Property(chess_engine_boost INSTALL_DIR) # change to BOOST_INSTALL_DIR
 set(BOOST_INSTALL_DIR ${INSTALL_DIR})
-include_directories(${BOOST_INSTALL_DIR}/include)
-link_directories(${BOOST_INSTALL_DIR}/lib)
+
+add_library(boost INTERFACE)
+target_include_directories(boost INTERFACE ${BOOST_INSTALL_DIR}/include/)
+target_link_directories(boost INTERFACE ${BOOST_INSTALL_DIR}/lib)
--- test/constants_test.cpp
+++ test/constants_test.cpp
@@ -2,7 +2,7 @@
 
 #include "../include/board_constants.hpp"
 
-TEST(board_constants_test, test_board64) {
+TEST(board_constants_test, board64) {
   std::array<size_t, 64> expected{
       21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38,
       41, 42, 43, 44, 45, 46, 47, 48, 51, 52, 53, 54, 55, 56, 57, 58,
